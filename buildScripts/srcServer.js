4ae38d14afeb3599f9d04677bd4a17eec65f924d
Eric Kleinem <ndimoro@gmail.com>
2017-03-18 02:30:23 -0400

    messed up stuff when trying to import packages

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+const express = require("express");
+const path = require("path");
+const open = require("open");
+const webpack = require("webpack");
+const config = require("../webpack.config.dev");
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
